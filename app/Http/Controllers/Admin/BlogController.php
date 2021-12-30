a98a130cb77c0b8bc17d3939ea5d3062d5ded0b7
SD <shaillydixit999@gmail.com>
2021-12-30 21:39:08 +0530

    blog api created

---
+++ app/Http/Controllers/Admin/BlogController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Blog;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    public function CommentBlog(Request $request){
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $message = $request->input('message');
+
+        date_default_timezone_set('Asia/Kolkata');
+        $contact_date = date('d-m-Y');
+        $contact_time = date('h:i:sa');
+
+        $result = Blog::insert([
+            'name' => $name,
+            'email' => $email,
+            'message' => $message,
+            'contact_date' => $contact_date,
+            'contact_time' => $contact_time
+        ]);
+
+        return $result;
+     }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\BlogController;
 use App\Http\Controllers\Admin\ContactController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::post('/contact', [ContactController::class, 'Contact']);
+
+Route::post('/commentblog',[BlogController::class, 'CommentBlog']);
